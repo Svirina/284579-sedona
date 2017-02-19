63312fc193af310fc2f9cf44cf8cddbfb767a7f3
Svirina <nastya-svirina@mail.ru>
2017-02-19 16:40:31 +0300

    исправления

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var popupButton = document.querySelector('.modal__header');
 var popup = document.querySelector('.modal__popup');
-
+/*
 var minusAdult = document.querySelector('#minusAdult');
 var plusAdult = document.querySelector('#plusAdult');
 var minusChild = document.querySelector('#minusChild');
 var plusChild = document.querySelector('#plusChild');
 var numAdult = document.querySelector('#numAdult').value;
 var numChild = document.querySelector('#numChild').value;
-
+*/
 var map;
 
 popupButton.classList.toggle('modal__header--clicked');
@@ -16,12 +16,13 @@ popup.classList.add('modal__popup--nonactive');
 popup.classList.add('modal__popup--noanimation');
 
 popupButton.addEventListener('click', function(event) {
+	event.preventDefault();
     popupButton.classList.toggle('modal__header--clicked');
     popup.classList.remove('modal__popup--noanimation');
     popup.classList.toggle('modal__popup--active');
     popup.classList.toggle('modal__popup--nonactive');
 });
-
+/*
 minusAdult.addEventListener('click', function(event) {
     event.preventDefault();
     if (numAdult > 0) {
@@ -49,7 +50,7 @@ plusChild.addEventListener('click', function(event) {
     numChild++;
     document.querySelector('#numChild').value = numChild;
 });
-
+*/
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 34.9471509, lng: -111.6930732},
